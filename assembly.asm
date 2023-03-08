52e7569cdc32caad5aa59c5b1d320cc8241852fc
LucasBorges4 <Lucasborges-2013@hotmail.com>
2023-03-08 14:29:53 -0300

    08/03

---
+++ assembly.asm
@@ -0,0 +1,24 @@
+; Compilar - Transformar o programa para linguagem de maquina
+; para instalar o nasm no windows basta usar o comando via powershell : winget install nasm -i
+; nasm -f elf64 assembly.asm
+; ld -s -o assembly assembly.o
+; .data é constantes
+
+section .data 
+    msg db 'Hello World!'
+    tam equ $- msg
+
+section .text
+
+global _start
+
+_start:
+    mov EAX, 0x4 ; quero fazer uma saida
+    mov EBX, 0x1 ; na saida padrao
+    mov ECX, msg ; mostra a mensagem
+    mov EDX, tam ; tamanho é esse
+    int 0x80 ; "CAMBIO", executa.
+
+    mov EAX, 0x1 ; terminando o programa
+    mov EBX, 0x0 ; valor de retorno é 0
+    int 0x80
