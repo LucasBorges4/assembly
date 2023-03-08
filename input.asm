1f233c71dec70e0f91529ac03c0e82f95e702008
LucasBorges4 <Lucasborges-2013@hotmail.com>
2023-03-08 20:03:51 -0300

    08/03/2023

---
+++ input.asm
@@ -0,0 +1,41 @@
+segment .data
+         LF equ 0xA ; line feed, quebra de linha
+         NULL equ 0xD;
+         SYS_CALL equ 0x80 ; envia informação ao SO
+    ; -----------------------------------------------
+    ; EAX
+         SYS_EXIT equ 0x1; Codigo de chamada finalizador
+         SYS_WRITE equ 0x4; operação de escrita
+         SYS_READ equ 0x3; operação de leitura
+    ; EBX 
+         RETURN_EXIT equ 0x0; Operação de sucesso, pode ser com qualquer valor.
+         STD_IN equ 0x0; Entrada Padrão [em EBX]
+         STD_OUT equ 0x1; Saída Padrão [em EBX]
+
+section .data
+         msg db "Entre com seu nome:", LF, NULL
+         tam equ $- msg
+
+section .bss
+    nome resb 1 ; (entrada)
+
+section .text
+
+global _start
+    ;label
+
+_start:
+         mov EAX, SYS_WRITE
+         mov EBX, STD_OUT
+         mov ECX, msg
+         mov EDX, tam
+         int SYS_CALL ;executa a operação
+
+         mov EAX, SYS_READ
+         mov EBX, STD_IN
+         mov ECX, nome (entrada)
+         mov EDX, 0xA
+         int SYS_CALL ;executa a operação
+
+         mov EAX, SYS_EXIT
+         mov EBX, RET_EXIT
